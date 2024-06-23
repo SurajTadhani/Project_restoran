cae2fc68e8c1f72d44b1d59df91e380b50626e1e
Suraj <surajtadhani@gmail.com>
2024-06-23 19:55:20 +0530

    Changing in index.html and index.js

--- js/index.js
+++ js/index.js
@@ -4,3 +4,32 @@ Aos.init({
   duration: 3000,
   offset: 120,
 });
+
+
+
+function initializeSlider(slider) {
+  const time = slider.dataset.sheetTime || 3000;
+  const inputs = slider.querySelectorAll("input");
+  let index = 0;
+  let intervalId;
+
+  function updateInputState() {
+    inputs[index].checked = false;
+    index = (index + 1) % inputs.length;
+    inputs[index].checked = true;
+  }
+
+  function startInterval() {
+    index = Array.from(inputs).findIndex((input) => input.checked) || 0;
+    intervalId = setInterval(updateInputState, time);
+  }
+
+  const stopInterval = () => clearInterval(intervalId);
+
+  startInterval();
+
+  slider.addEventListener("mouseenter", stopInterval);
+  slider.addEventListener("mouseleave", startInterval);
+}
+
+document.querySelectorAll("[data-sheet-time]").forEach(initializeSlider);
